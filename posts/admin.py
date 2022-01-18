6019cbde24c914a07e26fb6867b7154a5bfdf946
Brandon328 <70345941@continental.edu.pe>
2022-01-18 10:29:54 -0500

    clase de forms

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,19 @@
+# Django
+from atexit import register
 from django.contrib import admin
 
-# Register your models here.
+# Posts models
+from posts.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post admin."""
+    list_display = ('pk', 'user', 'title', 'photo')
+    list_display_links = ('pk', 'user')
+    list_filter = (
+        'created',
+        'modified',
+        )
+
+    readonly_fields = ('created', 'modified')
