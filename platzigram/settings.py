20b2bd129ed2e253d07296a8cad9d02af3fee189
Brandon328 <70345941@continental.edu.pe>
2022-01-18 17:04:26 -0500

    cambios...

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -23,8 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!ssnk!@*%szc)xu^-0c2mz5ejou9wx!r$#!c$+%)2=2c2u1pqt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
